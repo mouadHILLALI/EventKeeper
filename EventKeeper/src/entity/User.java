4c14ad8baf28978393467129b3b8aaf77db7d8bf
mouadHILLALI <mauadtoto001@gmail.com>
2024-08-30 16:34:35 +0100

    EV-3 project config

--- EventKeeper/src/entity/User.java
+++ EventKeeper/src/entity/User.java
@@ -1,4 +1,23 @@
 package entity;
 
 public class User {
+    private int id ;
+    private String username;
+
+    public User(int id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
 }
